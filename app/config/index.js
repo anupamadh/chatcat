05202f3f6fe0d480abee3e2532a1e8e9583ea68e
Anupama Dhir <sabhssabhs@yahoo.com>
2016-10-03 16:39:09 +0800

    changed config file

--- app/config/index.js
+++ app/config/index.js
@@ -6,8 +6,8 @@ if(process.env.NODE_ENV === 'production'){
     dbURI: process.env.dbURI,
     sessionSecret: process.env.sessionSecret,
     fb: {
-      clientID: process.env.fbclientID,
-      clientSecret: process.env.fbclientSecret,
+      clientID: process.env.fbClientID,
+      clientSecret: process.env.fbClientSecret,
       callbackURL: process.env.host + "/auth/facebook/callback",
       "profileFields": ["id", "displayName", "photos"]
     }
